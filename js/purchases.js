fd3cdf405cc9da6ace25e87c8e9867c61d2bbf31
sherifshiko <szshefo@gmail.com>
2025-11-16 03:00:36 -0800

    first commit

--- js/purchases.js
+++ js/purchases.js
@@ -13,9 +13,9 @@ for (let product of storedCard) {
     purchasesContant.insertAdjacentHTML('beforeend', `
 
                                 <div class="col-10">
-                <div class="d-flex justify-content-between border border-3 border-light p-5">
+                <div class="d-flex flex-column justify-content-between text-center border border-3 border-light p-5">
                     <div>
-                        <img src="${image}" class="w-100" alt="${title}">
+                        <img src="${image}" class="w-50" alt="${title}">
                     </div>
                     <div class="text-white text-center">
                         <h2>
@@ -42,10 +42,10 @@ for (let product of storedCard) {
 
 purchasesContant.insertAdjacentHTML('beforeend', `
                                 <div class="col-2 position-relative">
-                <div class="text-white text-center position-fixed check ">
+                <div class=" text-center position-fixed check ">
                     <h2>The check</h2>
                     <h3 class="my-5">Total</h3>
-                    <h4 class="text-success my-5 fs-1">${totalPrice}</h4>
+                    <h4 class="text-info my-5 fs-1">${totalPrice} $</h4>
                     <h5 >
                         <span class="btn btn-warning my-3">Cash payment</span>
                         <span class="btn btn-success">Cash on delivery</span>
